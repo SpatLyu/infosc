264c9d5c1f5448ec38f745e9f3cd114f40ea40ba
SpatLyu <lyu.geosocial@gmail.com>
2025-04-24 21:00:13 +0800

    add rcpp wrappers

---
+++ src/InfoTheoryExp.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include "InfoTheory.hpp"
+// 'Rcpp.h' should not be included and correct to include only 'RcppArmadillo.h'.
+// #include <Rcpp.h>
+#include <RcppArmadillo.h>
+// [[Rcpp::depends(RcppArmadillo)]]
+
+// [[Rcpp::export]]
+double RcppDiscEntropy(Rcpp::RObject vec, double base = 2) {
+  if (TYPEOF(vec) == REALSXP) {
+    std::vector<double> input = Rcpp::as<std::vector<double>>(vec);
+    return Rcpp::wrap(DiscEntropy(input, base));
+  } else if (TYPEOF(vec) == INTSXP) {
+    std::vector<int> input = Rcpp::as<std::vector<int>>(vec);
+    return Rcpp::wrap(DiscEntropy(input, base));
+  } else {
+    Rcpp::stop("Unsupported vector type. Must be numeric or integer.");
+  }
+}
