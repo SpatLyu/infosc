2314db03fe983b5784ff4999d9b1067ed297e13e
SpatLyu <lyu.geosocial@gmail.com>
2025-05-04 17:02:51 +0800

    update information theory sources

--- src/InfoTheoryExp.cpp
+++ src/InfoTheoryExp.cpp
@@ -35,3 +35,73 @@ double RcppDiscJoinEntropy(const Rcpp::NumericMatrix& mat,
   }
   return DiscJoinEntropy(cppMat,col_std,base);
 }
+
+// [[Rcpp::export]]
+double RcppDiscMI(const Rcpp::NumericMatrix& mat,
+                  const Rcpp::IntegerVector& columns1,
+                  const Rcpp::IntegerVector& columns2,
+                  double base = 10){
+  int numRows = mat.nrow();
+  int numCols = mat.ncol();
+
+  // Convert Rcpp NumericMatrix to std::vector<std::vector<double>>
+  std::vector<std::vector<double>> cppMat(numRows, std::vector<double>(numCols));
+  for (int i = 0; i < numRows; ++i) {
+    for (int j = 0; j < numCols; ++j) {
+      cppMat[i][j] = mat(i, j);
+    }
+  }
+
+  // Convert Rcpp IntegerVector to std::vector<int>
+  std::vector<int> col1 = Rcpp::as<std::vector<int>>(columns1);
+  for (size_t i = 0; i < col1.size(); ++i) {
+    if (col1[i] < 1 || col1[i] > numCols) {
+      Rcpp::stop("Each index in 'columns1' must be between 1 and %d (inclusive).", numCols);
+    }
+    col1[i] -= 1;
+  }
+  std::vector<int> col2 = Rcpp::as<std::vector<int>>(columns2);
+  for (size_t i = 0; i < col2.size(); ++i) {
+    if (col2[i] < 1 || col2[i] > numCols) {
+      Rcpp::stop("Each index in 'columns2' must be between 1 and %d (inclusive).", numCols);
+    }
+    col2[i] -= 1;
+  }
+
+  return columns(cppMat,col1,col2,base);
+}
+
+// [[Rcpp::export]]
+double DiscCE(const Rcpp::NumericMatrix& mat,
+              const Rcpp::IntegerVector& target_columns,
+              const Rcpp::IntegerVector& conditional_columns,
+              double base = 10){
+  int numRows = mat.nrow();
+  int numCols = mat.ncol();
+
+  // Convert Rcpp NumericMatrix to std::vector<std::vector<double>>
+  std::vector<std::vector<double>> cppMat(numRows, std::vector<double>(numCols));
+  for (int i = 0; i < numRows; ++i) {
+    for (int j = 0; j < numCols; ++j) {
+      cppMat[i][j] = mat(i, j);
+    }
+  }
+
+  // Convert Rcpp IntegerVector to std::vector<int>
+  std::vector<int> col1 = Rcpp::as<std::vector<int>>(target_columns);
+  for (size_t i = 0; i < col1.size(); ++i) {
+    if (col1[i] < 1 || col1[i] > numCols) {
+      Rcpp::stop("Each index in 'target_columns' must be between 1 and %d (inclusive).", numCols);
+    }
+    col1[i] -= 1;
+  }
+  std::vector<int> col2 = Rcpp::as<std::vector<int>>(conditional_columns);
+  for (size_t i = 0; i < col2.size(); ++i) {
+    if (col2[i] < 1 || col2[i] > numCols) {
+      Rcpp::stop("Each index in 'conditional_columns' must be between 1 and %d (inclusive).", numCols);
+    }
+    col2[i] -= 1;
+  }
+
+  return DiscCE(cppMat,col1,col2,base);
+}
